140b19c7759455aaac28e09bd0fae267abcb6229
Eastglint <55050063+eastglint@users.noreply.github.com>
2024-01-22 03:25:56 +0700

    Update lora.py

--- lora_diffusion/lora.py
+++ lora_diffusion/lora.py
@@ -50,7 +50,7 @@ class LoraInjectedLinear(nn.Module):
         nn.init.normal_(self.lora_down.weight, std=1 / r)
         nn.init.zeros_(self.lora_up.weight)
 
-    def forward(self, input):
+    def forward(self, input, scale: float = 1.0):
         return (
             self.linear(input)
             + self.dropout(self.lora_up(self.selector(self.lora_down(input))))
@@ -127,7 +127,7 @@ class LoraInjectedConv2d(nn.Module):
         nn.init.normal_(self.lora_down.weight, std=1 / r)
         nn.init.zeros_(self.lora_up.weight)
 
-    def forward(self, input):
+    def forward(self, input, scale: float = 1.0):
         return (
             self.conv(input)
             + self.dropout(self.lora_up(self.selector(self.lora_down(input))))
